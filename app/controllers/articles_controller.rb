f20452d7bb80e6cb4f317e38b9ba42fd43b4835e
Mohammad Umer <mohammadumer1991@gmail.com>
2015-10-29 15:45:18 +0500

    Enter data as a JS format

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,26 +1,26 @@
 class ArticlesController < ApplicationController
 	http_basic_authenticate_with name: "dhh", password: "secret", except: [:index, :show]
 	def index
-    @articles = Article.all
-  end
+	@articles = Article.all
+	end
 	def show
-    @article = Article.find(params[:id])
-  end
+	@article = Article.find(params[:id])
+	end
 	def new
 		@article = Article.new
 	end
 	def edit
-    @article = Article.find(params[:id])
-  end
+	@article = Article.find(params[:id])
+	end
 	def create
 	  @article = Article.new(article_params)
 	  if @article.save
 	    redirect_to @article
 	  else
 	    render 'new'
-  	end
-  end
-  def update
+		  end
+	end
+	def update
 	  @article = Article.find(params[:id])
 	 
 	  if @article.update(article_params)
@@ -36,7 +36,7 @@ class ArticlesController < ApplicationController
 	  redirect_to articles_path
 	end
 	private
-  def article_params
-    params.require(:article).permit(:title, :text)
-  end
+	def article_params
+	params.require(:article).permit(:title, :text)
+end
 end
--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -3,7 +3,10 @@ class CommentsController < ApplicationController
   def create
     @article = Article.find(params[:article_id])
     @comment = @article.comments.create(comment_params)
-    redirect_to article_path(@article)
+    respond_to do |format|
+        format.html { redirect_to post_path(@post) }
+        format.js 
+    end
   end
   def destroy
     @article = Article.find(params[:article_id])
